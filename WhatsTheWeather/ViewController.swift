e1b009b9b9990f0789bdff8326e816efc28d79c0
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-09-13 14:32:50 -0500

    Implement UI.

--- WhatsTheWeather/ViewController.swift
+++ WhatsTheWeather/ViewController.swift
@@ -4,17 +4,22 @@
 //
 //  Created by Charles Martin Reed on 9/13/18.
 //  Copyright © 2018 Charles Martin Reed. All rights reserved.
-//
+// BACKGROUND IMAGE, "Calm Sky During Daytime" by Kendrick Mills, @kendrickmills: https://unsplash.com/@kenrickmills
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func searchButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
